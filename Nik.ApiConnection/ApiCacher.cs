799572ff8aa0cca4543e33eab96a21405d35a3f3
Ehsan Faramarzi <netpro@email.com>
2024-04-20 22:23:44 +0200

    added lock for api cacher

--- Nik.ApiConnection/ApiCacher.cs
+++ Nik.ApiConnection/ApiCacher.cs
@@ -3,36 +3,43 @@
 public sealed class ApiCacher : IApiCacher
 {
     private List<ApiCache> cache = new();
+    private readonly object cacheLock = new();
 
     public void Create(string configKey, string action, object request, object response, TimeSpan interval)
     {
-        cache.RemoveAll(item =>
-            item.ConfigKey == configKey &&
-            item.Action == action &&
-            item.Request == request
-        );
-
-        cache.Add(new()
+        lock (cacheLock)
         {
-            ConfigKey = configKey,
-            Action = action,
-            Request = request,
-            Response = response
-        });
+            cache.RemoveAll(item =>
+                item.ConfigKey == configKey &&
+                item.Action == action &&
+                item.Request == request
+            );
+
+            cache.Add(new()
+            {
+                ConfigKey = configKey,
+                Action = action,
+                Request = request,
+                Response = response
+            });
+        }
     }
 
     public object? Get(string configKey, string action, object request, TimeSpan interval)
     {
-        cache.RemoveAll(item =>
-            item.ConfigKey == configKey &&
-            item.Action == action &&
-            item.CreateTime < DateTime.Now - interval
-        );
+        lock (cacheLock)
+        {
+            cache.RemoveAll(item =>
+                item.ConfigKey == configKey &&
+                item.Action == action &&
+                item.CreateTime < DateTime.Now - interval
+            );
 
-        return cache.FirstOrDefault(item =>
-            item.ConfigKey == configKey &&
-            item.Action == action &&
-            item.Request == request
-        )?.Response;
+            return cache.FirstOrDefault(item =>
+                item.ConfigKey == configKey &&
+                item.Action == action &&
+                item.Request == request
+            )?.Response;
+        }
     }
 }
